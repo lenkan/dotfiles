7a8e5106e061076ec74aea62b873f9351635513d
lenkan <dlenksjo@gmail.com>
2017-10-15 19:33:28 +0200

    Added zshrc

--- copy.sh
+++ copy.sh
@@ -3,3 +3,4 @@ cp ./config/i3/config ~/.config/i3/
 cp ./config/polybar/config ~/.config/polybar/
 cp ./config/Code/User/*.json ~/.config/Code/User/
 cp ./bin/*.sh ~/bin/
+cp ./zsh/.zshrc ~/
--- emacs/vue/packages.el
+++
@@ -1,8 +0,0 @@
-(setq vue-packages
-  '(
-    (vue-mode :location (recipe :fetcher github :repo "codefalling/vue-mode"))
-	))
-
-
-(defun vue/init-vue-mode ()
-    (use-package vue-mode))
