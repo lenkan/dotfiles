32c519fd1f13574d921430549208e37a61103b48
lenkan <dlenksjo@gmail.com>
2017-10-22 15:18:45 +0200

    Update keybindings

--- copy.sh
+++
@@ -1,8 +0,0 @@
-# Copy files 
-cp ./config/i3/config ~/.config/i3/
-cp ./config/polybar/config ~/.config/polybar/
-cp ./config/polybar/*.sh ~/.config/polybar/
-cp ./config/Code/User/*.json ~/.config/Code/User/
-cp ./bin/*.sh ~/bin/
-cp ./zsh/.zshrc ~/
-cp ./zsh/*.zsh ~/.zplug/
