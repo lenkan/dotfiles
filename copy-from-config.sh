a9777192f2d8b586e2e1d72b9b301a38cc725d1e
lenkan <dlenksjo@gmail.com>
2017-10-24 08:09:08 +0200

    Fixed scripts

---
+++ copy-from-config.sh
@@ -0,0 +1,5 @@
+# Copy files 
+cp ~/.config/i3/config ./config/i3/
+cp ~/.config/polybar/config ./config/polybar/
+cp ~/.config/polybar/*.sh ./config/polybar/ 
+cp ~/.config/Code/User/*.json ./config/Code/User/
