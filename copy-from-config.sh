a0250cf0b82ba6b2ffe0fc7b4f235204b826c994
lenkan <dlenksjo@gmail.com>
2017-11-17 14:41:54 +0100

    Some updates

--- copy-from-config.sh
+++ copy-from-config.sh
@@ -3,3 +3,4 @@ cp ~/.config/i3/config ./config/i3/
 cp ~/.config/polybar/config ./config/polybar/
 cp ~/.config/polybar/*.sh ./config/polybar/ 
 cp ~/.config/Code/User/*.json ./config/Code/User/
+cp ~/.zshrc ./zsh/.zshrc
