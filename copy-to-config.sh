9ffc3347146fde7e638b0264cea93b80479d548f
lenkan <dlenksjo@gmail.com>
2017-11-20 01:14:41 +0100

    Added compton

--- copy-to-config.sh
+++ copy-to-config.sh
@@ -1,5 +1,6 @@
-# Copy files 
+#!/bin/bash
 cp ./config/i3/config ~/.config/i3/
+cp ./config/compton/config ~/.config/compton/
 cp ./config/polybar/config ~/.config/polybar/
 cp ./config/polybar/*.sh ~/.config/polybar/
 cp ./config/Code/User/*.json ~/.config/Code/User/
