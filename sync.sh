87c0ab39a53149eef7cc39da07914ffa8e139b5b
lenkan <dlenksjo@gmail.com>
2019-04-15 08:57:49 +0200

    Create fehbg

--- sync.sh
+++ sync.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-ln -svf $(pwd)/.zplug/plugins.zsh ~/.zplug/plugins.zsh
-
-ln -svf $(pwd)/Pictures/wp/wallpaper.jpg ~/Pictures/wp/wallpaper.jpg
-
 for filename in $DIR/bin/*; do
   ln -svf $filename ~/bin/$(basename $filename)
 done
@@ -15,17 +11,14 @@ for filename in $DIR/.*; do
   fi
 done
 
+ln -svf $(pwd)/.zplug/plugins.zsh ~/.zplug/plugins.zsh
+ln -svf $(pwd)/Pictures/wp/wallpaper.jpg ~/Pictures/wp/wallpaper.jpg
+
 ln -svf $(pwd)/.config/Code/User/keybindings.json ~/.config/Code/User/keybindings.json
 ln -svf $(pwd)/.config/Code/User/settings.json ~/.config/Code/User/settings.json
 ln -svf $(pwd)/.config/Code/User/locale.json ~/.config/Code/User/locale.json
 
 ln -svf $(pwd)/.config/i3/config ~/.config/i3/config
-
 ln -svf $(pwd)/.config/termite/config ~/.config/termite/config
-
 ln -svf $(pwd)/.config/pulse/default.pa ~/.config/pulse/default.pa
-
 ln -svf $(pwd)/.config/polybar/config ~/.config/polybar/config
-ln -svf $(pwd)/.config/polybar/bluetooth.sh ~/.config/polybar/bluetooth.sh
-ln -svf $(pwd)/.config/polybar/dropbox.sh ~/.config/polybar/dropbox.sh
-ln -svf $(pwd)/.config/polybar/launch.sh ~/.config/polybar/launch.sh
