fca8e8c577e59a95e217f9a67e5ea948a81c4179
lenkan <dlenksjo@gmail.com>
2019-07-06 09:54:25 +0200

    Fix monitor selection

---
+++ bin/monitor.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+external_monitor=$(xrandr | awk '/\ connected .*+/{print $1}' | tail -n 1)
+if [[ $external_monitor == 'eDP1' ]]
+then
+  exit 0
+fi
+
+if [[ $1 == 'off' ]]
+then
+  xrandr --output $external_monitor --off
+  exit 0
+fi
+
+if [[ $1 == 'above' ]]
+then
+  xrandr --output $external_monitor --preferred --above eDP1
+  exit 0
+fi
+
+if [[ $1 == 'below' ]]
+then
+  xrandr --output $external_monitor --preferred --below eDP1
+  exit 0
+fi
+
+if [[ $1 == 'same' ]]
+then
+  xrandr --output $external_monitor --same-as eDP1
+  exit 0
+fi
